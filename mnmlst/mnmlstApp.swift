16e56cab221890917792401b9e0579d892909dee
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-02-05 17:36:09 -0500

    Initial Commit

---
+++ mnmlst/mnmlstApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnmlstApp.swift
+//  mnmlst
+//
+//  Created by Rob Bryson on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct mnmlstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
